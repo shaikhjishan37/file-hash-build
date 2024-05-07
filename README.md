1fda851e0c539e82d9acad426206caf6cc3e2a6b
jshaikh <jshaikh@digite.com>
2024-05-07 13:38:53 +0530

    hasher base

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # file-hash-build
 File hasher based on content and impact on any referenced places in a repo
+
+### Command
+```
+npx hashfiles -folder=#FOLDERPATH# -fileExtension=#FILE-EXTENSION# -postProcessCleanUpPath=#PROCESS-FILE-PATH# -impactFilesPath=#IMPACTED-ROOT-FOLDER#
+```
+
+### -folder
+```
+Multiple args can be passed with same key.
+This is used to specify folder location where files are needed to be hashed.
+If multiple folder are needed pass same arg multiple times with path of all folders
+
+Example: -folder=/data/username/myprj
+```
+
+### -fileExtension
+
+```
+Multiple args can be passed with same key.
+This is used to specify which files extension or type are needed to be hashed.
+If multiple filetypes are needed to be hashed pass this argument multiple time in command.
+
+Example: -fileExtension=.js -fileExtension=.css
+```
+
+### -postProcessCleanUpPath
+```
+After files are hashed this needs to be updated in code base wherever they are needed.
+File paths have some root folder from where we refrence them, for for replace we want to remove extra path from file which needs to be cleaned up.
+
+Example: -folder=/data/user/myprj -postProcessCleanUpPath=/data/user/
+```
+
+### -impactFilesPath
+
+```
+Impacted files where new path need to updated can be provided on arg, if not provided root folder where command was run will be considered in use for this
+```
---
+++ index.js
@@ -0,0 +1,88 @@
+#! /usr/bin/env node
+
+console.log("File hasher Started!");
+
+const fs = require('fs');
+const path = require("path");
+const crypto = require('crypto');
+const { exec } = require('child_process');
+const { promisify } = require('util');
+const exec_async = promisify(exec);
+
+const getHash = ( content ) => {				
+  const hash = crypto.createHash('md5');
+  //passing the data to be hashed
+  const data = hash.update(content, 'utf-8');
+  //Creating the hash in the required format
+  const gen_hash = data.digest('hex');
+
+  return gen_hash;
+}
+
+const processFile = (file, filePath, extensions, filePathCleanUp, impactFiles = '*.*') => {
+  const fileExtension = path.extname(filePath);
+
+  if ( extensions.includes(fileExtension) ) {
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const filehash = getHash(fileContent);
+
+    if ( !filePath.includes(filehash) ) {
+      const fileNewName = filePath.replace(fileExtension, `.${filehash}${fileExtension}`);
+
+      fs.rename(filePath, fileNewName, function(err) {
+        if ( err ) console.log('ERROR: ' + err);
+      });
+
+      const filePathAfterCleanup = filePath.replace(filePathCleanUp, '');
+      const fileNewNameAfterCleanup = fileNewName.replace(filePathCleanUp, '');
+      const sedCommand = `grep -rl '${filePathAfterCleanup}' '${impactFiles}' | xargs sed -i 's|${filePathAfterCleanup}|${fileNewNameAfterCleanup}|g'`;
+
+      console.log(sedCommand)
+
+      exec_async(sedCommand)
+      .catch(err => {console.log(err)})
+    }
+  }
+}
+
+const procesFolders = (folderPaths, extensions, filePathCleanUp, impactFiles) => {
+  folderPaths.forEach( (folderPath) => {
+    const folders = fs.readdirSync(folderPath);
+
+    folders.forEach((folderContent) => {
+      const childPath = `${folderPath}/${folderContent}`;
+      const stats = fs.statSync(childPath);
+
+      if ( stats.isFile() ) {
+        processFile(folderContent, childPath, extensions, filePathCleanUp, impactFiles);
+      } else if ( stats.isDirectory() ) {
+        procesFolders([childPath], extensions, filePathCleanUp, impactFiles);
+      }
+    })
+  } );
+}
+
+((folderPathsKey, extensionsKey, postProcessCleanUpPath, impactFilesPath) => {
+  const folderPaths = [];
+  const extensions = [];
+  let filePathCleanUp = '';
+  let impactFiles;
+
+  process.argv.forEach((userInput) => {
+    if (userInput.includes('=') && userInput.startsWith('-')) {
+      const [key, value] = userInput.split('=');
+
+      if ( key.toLocaleLowerCase() === folderPathsKey.toLocaleLowerCase() ) {
+        folderPaths.push(value);
+      } else if ( key.toLocaleLowerCase() === extensionsKey.toLocaleLowerCase() ) {
+        extensions.push(value);
+      } else if ( key.toLocaleLowerCase() === postProcessCleanUpPath.toLocaleLowerCase() ) {
+        filePathCleanUp = value;
+      }  else if ( key.toLocaleLowerCase() === impactFilesPath.toLocaleLowerCase() ) {
+        impactFiles = value;
+      }
+    }
+  });
+
+  procesFolders(folderPaths, extensions, filePathCleanUp, impactFiles);
+})('-folder', '-fileExtension', '-postProcessCleanUpPath', '-impactFilesPath');
